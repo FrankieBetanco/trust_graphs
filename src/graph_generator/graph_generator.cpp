2eaf6ce1159c1a8eb07ca1564aedcd41b78c7d8e
frankie <frankie@halleck.eduroam-corewlcs-10.okstate.edu>
2019-10-07 14:00:10 -0500

    make printing prettier
    fix bug with graph representation

--- src/graph_generator/graph_generator.cpp
+++ src/graph_generator/graph_generator.cpp
@@ -1,4 +1,5 @@
 #include <iostream> 
+#include <iomanip>
 #include <string> 
 #include <fstream>
 #include <sstream>
@@ -30,10 +31,10 @@ void graph::node_view(int from, int to) {
   permissions = adj[to][from];
   for (int i = 0; i < 9; i++) {
     if ( permissions & (1 << i) ) {
-      attributes += users[to]->attributes[i];
+      attributes += "X";
     }
     else { 
-      attributes += "X";
+      attributes += users[to]->attributes[i];
     }
     if ( i + 1 < 9 ) {
       attributes += ", ";
@@ -83,19 +84,52 @@ void graph::construct_graph() {
     for (int j = 0; j < n_users; j++) {
       adj[i][j] = (unsigned short) stoi(users[i]->attributes[PERMISSIONS]);
     }
-    adj[i][i] = 511; 
+    adj[i][i] = 0; 
   }
 }
 
 void graph::print_binary_graph(int attribute_num) {
-  cout << "Attribute " << attribute_num << '\n';
+  cout << "Attribute " << attribute_num << ": ";
+  switch (attribute_num) {
+    case 0: cout << "First Name\n"; 
+            break;
+    case 1: cout << "Last Name\n";
+            break;
+    case 2: cout << "Age\n"; 
+            break;
+    case 3: cout << "Gender\n"; 
+            break;
+    case 4: cout << "Primary Language\n"; 
+            break;
+    case 5: cout << "SNS ID\n"; 
+            break;
+    case 6: cout << "Email Address\n"; 
+            break;
+    case 7: cout << "Services\n"; 
+            break;
+    case 8: cout << "Groups\n"; 
+            break;
+    default: 
+            cout << "Invalid attribute number\n";
+            return;
+  }
+  cout << setw(6) << " ";
   for (int i = 0; i < adj.size(); i++) {
+    cout << setw(5) << i; 
+  }
+  cout << "\n";;
+  for (int i = 0; i < adj.size() * 5 + 10; i++) {
+    cout << "-"; 
+  }
+  cout << "\n";
+  for (int i = 0; i < adj.size(); i++) {
+    cout << "Node #" << setw(4) << left << to_string(i) + ":"; 
     for (int j = 0; j < adj[i].size(); j++) {
       if ( adj[i][j] & (1 << attribute_num) ) {
-        cout << "1 ";
+        cout << setw(5) << "1";
       }
       else {
-        cout << "0 ";
+        cout << setw(5) << "0 ";
       }
     }
     cout << "\n";
