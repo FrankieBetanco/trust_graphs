0fee006ca7f335682f13e50eb05f647cda62af8c
frankie <frankie@halleck.eduroam-corewlcs-10.okstate.edu>
2019-10-07 13:37:59 -0500

    add option for printing a graph for a single attribute

--- src/graph_generator/graph_generator.h
+++ src/graph_generator/graph_generator.h
@@ -17,9 +17,9 @@ public:
   void node_view(int from, int to);
   void node_view_all(int from); 
   void add_privacy_edge(int attribute_num, int from, int to);
+  void print_binary_graph(int attribute_num);
 private: 
   void parse_input();
   void construct_graph();
-  void print_binary_graph(int attribute_num);
 };
 
--- src/graph_generator/test.cpp
+++ src/graph_generator/test.cpp
@@ -5,16 +5,19 @@ using namespace std;
 
 int main(int argc, char **argv) {
   string filename; 
+  string action;
+
   if ( argc < 3 ) {
     cout << "usage: graph_gen [filename] [action]\n";
-    cout << "actions: v\n";
+    cout << "actions: v, p\n";
     return -1;
   }
   filename = argv[1];
+  action = argv[2];
 
   graph g(filename);
 
-  if ( strcmp(argv[2], "v") == 0 ) {
+  if ( action == "v" ) {
     if ( argc == 4 ) {
       g.node_view_all(stoi(argv[3]));
     } else if ( argc >= 5 ) {
@@ -26,11 +29,19 @@ int main(int argc, char **argv) {
       cout << " -if action is v, then specify one or more nodes\n"; 
       cout << " -if a single node is specified, then that nodes view of all\n"; 
       cout << "  other nodes in the graph is shown\n"; 
-      cout << " -if more than one node is specified, then that nodes view of\n";
-      cout << "  all of the nodes following the first will be shown\n"; 
+      cout << " -if more than one node is specified, then that nodes view\n";
+      cout << "  of all of the nodes following the first will be shown\n"; 
       cout << "Example Usages:\n"; 
       cout << "graph_gen [filename] v 10\n"; 
       cout << "graph_gen [filename] v 10 15 20 30\n";
+    }
+  }
+
+  if ( action == "p" ) {
+    if ( argc == 4 ) {
+      g.print_binary_graph(stoi(argv[3]));
+    } else {
+      cout << "Usage: graph_gen [filename] p [attribute num]\n";
     }
   }
 
