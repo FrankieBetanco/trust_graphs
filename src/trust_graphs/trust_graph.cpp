8112c98af60872e3de4ad564369939410cc6ccff
frankie <frankie@halleck.eduroam-corewlcs-10.okstate.edu>
2019-11-05 12:28:43 -0600

    create trust_graphs for pa2

---
+++ src/trust_graphs/trust_graph.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <queue> 
+#include <algorithm> 
+#include <cstdio>
+
+#include "graph_generator.h"
+using namespace std;
+
+int main(int argc, char **argv) {
+}
